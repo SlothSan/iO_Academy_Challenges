e96fae970b04c3bd0f5027fb4cf36160119589e4
Mike Oram <Mikeoram01@gmail.com>
2022-07-19 20:42:27 +0100

    Moved back from docker, kinda of working ish ? need to make a front-end + get someone to sanity check my code.

--- PHP_Posts_And_Railings/index.php
+++ PHP_Posts_And_Railings/index.php
@@ -2,25 +2,47 @@
 //Railing is 1.5M long
 //post is 10cm and every Railing needs to be connected too 2 posts. 
 
-function fenceLength(float $length) {
-    $railing = 150;
-    $post = 10;
-    $railPostArray = [];
+echo '<p>Hello World</p>';
+
+$railingLength = 1.5;
+$postLength = 0.1;
+
+echo "<p>$railingLength m is the length of a railing & $postLength m is the length of a post</p>";
+
+
+
+function railingsAndPostsByLength(float $length, float $postLength = 0.1, float $railingLength = 1.5): array
+{
+    $testArray = [];
     $postCount = 1;
     $railingCount = 0;
-    $length -= $post;
+    $length -= $postLength;
     do {
-        $length - ($railing + $post);
-        $postCount++;
         $railingCount++;
+        $postCount++;
+        $length -= ($postLength + $railingLength);
     } while ($length > 0);
-
-    $railPostArray[] = $railingCount;
-    $railPostArray[] = $postCount;
-
-    print_r($railPostArray);
+    $testArray['postCount'] = $postCount;
+    $testArray['railingCount'] = $railingCount;
+    return $testArray;
 }
 
-echo fenceLength(1000);
+$testArray2 = railingsAndPostsByLength(17.00);
+echo "<p>For the length you entered you will need:</p>";
+echo "<p>You will need " . $testArray2['postCount'] . " posts.</p> <p>And you will need " . $testArray2['railingCount'] . " railings.";
 
+function lengthByAmountOfPostsAndRailings(int $amountOfPosts, int $amountOfRailings, float $postLength = 0.1, float $railingLength = 1.5): float
+{
+    echo "<p>You inputed $amountOfPosts posts & $amountOfRailings railings";
+    $length = 0.00;
+    $amountOfPosts--;
+    $length += $postLength; //Assume 1 post always
+    do {
+        $amountOfPosts--;
+        $amountOfRailings--;
+        $length += ($postLength + $railingLength);
+    } while ($amountOfPosts > 0 && $amountOfRailings > 0);
+    return $length;
+}
+echo "<p> your fence will be " . lengthByAmountOfPostsAndRailings(200, 300) . "meters long </p>";
 ?>
