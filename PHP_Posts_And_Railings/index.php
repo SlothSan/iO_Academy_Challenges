dccb76ab19926a91d57b7478222d7607aaaeb529
Mike Oram <Mikeoram01@gmail.com>
2022-07-19 19:36:56 +0100

    PHP testing for challenge - commiting before moving to docker.

--- PHP_Posts_And_Railings/index.php
+++ PHP_Posts_And_Railings/index.php
@@ -1,3 +1,26 @@
 <?php
-//2 posts per rail always
-//
+//Railing is 1.5M long
+//post is 10cm and every Railing needs to be connected too 2 posts. 
+
+function fenceLength(float $length) {
+    $railing = 150;
+    $post = 10;
+    $railPostArray = [];
+    $postCount = 1;
+    $railingCount = 0;
+    $length -= $post;
+    do {
+        $length - ($railing + $post);
+        $postCount++;
+        $railingCount++;
+    } while ($length > 0);
+
+    $railPostArray[] = $railingCount;
+    $railPostArray[] = $postCount;
+
+    print_r($railPostArray);
+}
+
+echo fenceLength(1000);
+
+?>
