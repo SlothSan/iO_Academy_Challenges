a2d1434d6ca5dfffc3459f111e91587579d5c9ca
Mike Oram <Mikeoram01@gmail.com>
2022-08-14 12:55:41 +0100

    In and working. setting up stop points for when you reach the end of the Div.

--- JS-Game-POC/main.js
+++ JS-Game-POC/main.js
@@ -2,8 +2,24 @@ const container = document.querySelector('#container');
 const move = document.querySelector('#move-item');
 
 //Gets the property Left from the move element. 
-let positionLeft = window.getComputedStyle(move).getPropertyValue('left');
 
 console.log(container);
 console.log(move);
-console.log(positionLeft)
+
+//Detect KeyPresses. 
+document.onkeydown = detectKey;
+
+function detectKey(event) {
+    let positionLeft = move.style.left;
+     event = event || window.event;
+     if (event.keyCode == '37') {
+        // left arrow
+        move.style.left  = (parseInt(positionLeft) + 5) + 'px';
+        console.log(parseInt(positionLeft));
+     }
+     else if (event.keyCode == '39') {
+        // right arrows
+        move.style.left  = (parseInt(positionLeft) + 5) + 'px';
+        console.log(parseInt(positionLeft));
+     }
+ }
