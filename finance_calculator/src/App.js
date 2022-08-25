2b38eff693b32ee4d28ed0aff129b016c56a59b4
Mike Oram <Mikeoram01@gmail.com>
2022-08-25 20:30:09 +0100

    InfoContainer.js complete, ready to style.

--- finance_calculator/src/App.js
+++ finance_calculator/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import InfoContainer from './header/InfoContainer';
 import './App.css';
+
 
 function App() {
   return (
     <main className="App">
+      <InfoContainer />
     </main>
   );
 }
--- finance_calculator/src/header/InfoContainer.js
+++ finance_calculator/src/header/InfoContainer.js
@@ -1,5 +1,10 @@
 const InfoContainer = () => {
-
+    return (
+        <header className="infoContainer">
+            <h1 className="infoTitle">Finance Calculator</h1>
+            <p className="infoText">Fill out the form below and we will calculate exactly what you will pay for your tuition at iO Academy and how long it will take to pay that back.</p>
+        </header>
+    )
 }
 
 export default InfoContainer;
