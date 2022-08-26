24a8bc192bc6cd796c361668c4c240b3ff4d4476
Mike Oram <Mikeoram01@gmail.com>
2022-08-26 09:11:30 +0100

    More positioning changes and updated text in InfoContainer

--- finance_calculator/src/header/InfoContainer.js
+++ finance_calculator/src/header/InfoContainer.js
@@ -4,7 +4,7 @@ const InfoContainer = () => {
     return (
         <header className="infoContainer">
             <h1 className="infoTitle">Finance Calculator</h1>
-            <p className="infoText">Fill out the form below and we will calculate exactly what you will pay for your tuition at Mayden Academy and how long it will take to pay that back</p>
+            <p className="infoText">Fill out the form below and we will calculate exactly what you will pay for your tuition at iO Academy and how long it will take to pay that back</p>
             <img className='iOLogo' src={logo} alt="iO Logo" />
         </header>
     )
